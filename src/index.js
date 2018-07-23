becb1347371d45ad3e27a94a7fc2f4d12a84d623
snailuncle <1789500304@qq.com>
2018-07-24 02:09:39 +0800

    'daily-7-24-显示打包包主页是nodejsHelloWorld/douban/src/index.html'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-console.log(module.parent.filename+ "  调用了模块  ======")
-console.log(module.filename)
-import React from 'react'
-import {render} from 'react-dom'
-import {
-  BrowserRouter
-} from 'react-router-dom'
-import App from './app'
-console.log("src/index.js")
-const rootElement=document.getElementById('app')
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  rootElement
-)
+import './assets/common.sass'
 
+function changeTitle () {
+  window.$('#app').html('Parcel 打包包')
+}
+
+setTimeout(function () {
+  changeTitle()
+}, 2000)
