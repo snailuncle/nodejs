e423d9196629ee3276a16aeea95e4ac0d64e6768
snailuncle <1789500304@qq.com>
2018-07-24 11:08:53 +0800

    'daily-7-24-不显示首页两个字'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import './assets/common.sass'
+console.log(module.parent.filename+ "  调用了模块  ======")
+console.log(module.filename)
+import React from 'react'
+import {render} from 'react-dom'
+import {
+  BrowserRouter
+} from 'react-router-dom'
+import App from './app'
 
-function changeTitle () {
-  window.$('#app').html('Parcel 打包包')
-}
-
-setTimeout(function () {
-  changeTitle()
-}, 2000)
+const rootElement=document.getElementById('app')
+render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,rootElement
+)
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+console.log(module.parent.filename+ "  调用了模块  ======")
+console.log(module.filename)
+import AC from './components/async_load'
+
+export default[
+  {
+    name:'首页',
+    icon:'home',
+    path:'/',
+    component:AC(()=>import('./views/home'))
+  },
+  {
+    name:'详情页',
+    path:'/detail/:id',
+    component:AC(()=>import('./views/movie/detail'))
+  }
+]
